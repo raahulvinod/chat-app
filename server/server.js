b8238d7d50f9384d5a27bc8d0cacfdc3ee837f34
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-05-13 11:42:34 +0530

    feat: Initial set up for client and server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
