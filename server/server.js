b23d0b88b1a99615cf26ee335da8bc8930f170bc
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-05-22 16:00:07 +0530

    feat(server): add send message

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
-import authRoute from './routes/auth.route.js';
 import connectDb from './config/dbConnect.js';
+import authRoute from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
 
 dotenv.config();
 
@@ -10,8 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
+app.use('/api/messages', messageRoute);
 
 app.listen(PORT, () => {
   connectDb();
