e98f667fb028614b3dbd558ed7f5cda3b459ecee
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-05-25 11:17:57 +0530

    feat(client): add sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Signup />
+      <Home />
     </div>
   );
 }
