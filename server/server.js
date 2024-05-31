64ecfe88f6918e7ea7742cafa3582ec557e3d1ba
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-05-31 11:16:03 +0530

    feat(deployment); fix deployment issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 
 import connectDb from './config/dbConnect.js';
 import authRoute from './routes/auth.route.js';
@@ -13,20 +12,12 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
 app.use('/api/messages', messageRoute);
 app.use('/api/users', userRoute);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 server.listen(PORT, () => {
   connectDb();
