bbfe4d277db2facb96d0e8417004ac22b225d860
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-05-31 10:29:44 +0530

    feat(deployment): final deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_SERVER_DOMAIN || 'http://localhost:8000',
       },
     },
   },
